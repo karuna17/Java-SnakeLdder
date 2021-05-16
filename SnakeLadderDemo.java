d5c1fa0b5eb776474ef8521963577443c5be0b7c
karuna17 <karunakakade8@gmail.com>
2021-05-16 21:46:17 +0530

    Added: Check option for player

--- SnakeLadderDemo.java
+++ SnakeLadderDemo.java
@@ -1,11 +1,28 @@
 
 public class SnakeLadderDemo {
+	static int playerPosition = 0;
+	static final int noPlay = 0;
+	static final int ladder = 1;
+	static final int snake = 2;
 
 	public static void main(String[] args) {
-		int playerPosition = 0;
 		int rollDie = (int) (Math.floor(Math.random()*(6)+1));
-		System.out.println("OutCome: "+rollDie);
-		
-	}
+		int option = (int) ((Math.random()*10)%3);
 
+		switch(option) {
+		case noPlay: 
+			playerPosition = 0;
+			System.out.println("Player position is: "+playerPosition);
+			break;
+		case ladder:
+			playerPosition += rollDie;
+			System.out.println("Player position is: "+playerPosition);
+			break;
+		case snake:
+			playerPosition -= rollDie;
+			System.out.println("Player position is: "+playerPosition);
+			break;
+		}
+
+	}
 }
