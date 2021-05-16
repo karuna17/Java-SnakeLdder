03808fa77f5fae9173f5b6df287229f30af1f08c
karuna17 <karunakakade8@gmail.com>
2021-05-16 23:57:48 +0530

    Added: Exact Winning position is 100

--- SnakeLadderDemo.java
+++ SnakeLadderDemo.java
@@ -3,31 +3,42 @@ public class SnakeLadderDemo {
 	static final int noPlay = 0;
 	static final int ladder = 1;
 	static final int snake = 2;
+	static int playerPosition = 0;
+	int winPosition=100;
+	int option;
+	int rollDie;
+	public void dice() {
+		for(playerPosition=0; playerPosition < 100; playerPosition++) {
+			option = (int) ((Math.random()*10)%3);
+			rollDie = (int) (Math.floor(Math.random()*(6)+1));
 
-	public static void main(String[] args) {
-		int playerPosition = 0;
-		int winPosition=100;
-
-		for(playerPosition=0; playerPosition<winPosition; playerPosition++) {
-
-			int option = (int) ((Math.random()*10)%3);
-			int rollDie = (int) (Math.floor(Math.random()*(6)+1));
-			if(playerPosition < 0) {
-				switch(option) {
-				case noPlay: 
-					playerPosition = 0;
-					//System.out.println("Player position is: "+playerPosition);
-					break;
-				case ladder:
-					playerPosition += rollDie;
-					//System.out.println("Player position is: "+playerPosition);
-					break;
-				case snake:
-					playerPosition -= rollDie;
-					break;
-				}	
-			}
+			switch(option) {
+			case noPlay: 
+				playerPosition = 0;
+				break;
+			case ladder:
+				playerPosition += rollDie;
+				break;
+			case snake:
+				playerPosition -= rollDie;
+				break;
+			}	
 		}
 		System.out.println("Player position is: "+playerPosition);
+	}
+	
+	public static void main(String[] args) {
+		SnakeLadderDemo s = new SnakeLadderDemo();
+		System.out.println(playerPosition);
+		if(playerPosition > 100) {	
+			s.dice();
+			}else if(playerPosition == 100){
+				s.dice();
+			}else {
+				s.dice();
+			}
+				
+
+
 	}
 }
