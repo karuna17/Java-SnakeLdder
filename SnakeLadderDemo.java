c25b116fd5f48485b658570d0bc224ceb8bda53f
karuna17 <karunakakade8@gmail.com>
2021-05-16 23:05:58 +0530

    Added: Repeate Till Winning position is 100

--- SnakeLadderDemo.java
+++ SnakeLadderDemo.java
@@ -1,28 +1,33 @@
 
 public class SnakeLadderDemo {
-	static int playerPosition = 0;
 	static final int noPlay = 0;
 	static final int ladder = 1;
 	static final int snake = 2;
 
 	public static void main(String[] args) {
-		int rollDie = (int) (Math.floor(Math.random()*(6)+1));
-		int option = (int) ((Math.random()*10)%3);
+		int playerPosition = 0;
+		int winPosition=100;
 
-		switch(option) {
-		case noPlay: 
-			playerPosition = 0;
-			System.out.println("Player position is: "+playerPosition);
-			break;
-		case ladder:
-			playerPosition += rollDie;
-			System.out.println("Player position is: "+playerPosition);
-			break;
-		case snake:
-			playerPosition -= rollDie;
-			System.out.println("Player position is: "+playerPosition);
-			break;
+		for(playerPosition=0; playerPosition<winPosition; playerPosition++) {
+
+			int option = (int) ((Math.random()*10)%3);
+			int rollDie = (int) (Math.floor(Math.random()*(6)+1));
+			if(playerPosition < 0) {
+				switch(option) {
+				case noPlay: 
+					playerPosition = 0;
+					//System.out.println("Player position is: "+playerPosition);
+					break;
+				case ladder:
+					playerPosition += rollDie;
+					//System.out.println("Player position is: "+playerPosition);
+					break;
+				case snake:
+					playerPosition -= rollDie;
+					break;
+				}	
+			}
 		}
-
+		System.out.println("Player position is: "+playerPosition);
 	}
 }
