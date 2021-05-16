828354c3d07effb70f81343252acaf946fc91fab
karuna17 <karunakakade8@gmail.com>
2021-05-16 21:13:22 +0530

    Added: Roll Die to get number between 1 to 6

--- SnakeLadderDemo.java
+++ SnakeLadderDemo.java
@@ -2,8 +2,9 @@
 public class SnakeLadderDemo {
 
 	public static void main(String[] args) {
-
-		int playerPosition=0;
+		int playerPosition = 0;
+		int rollDie = (int) (Math.floor(Math.random()*(6)+1));
+		System.out.println("OutCome: "+rollDie);
 		
 	}
 
